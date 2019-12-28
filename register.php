30a9a89c4bb71b1ddc582db6982f9b7f7d5f4d88
Abubakar Oluyinka <lexico4real@gmail.com>
2019-12-28 12:51:21 +0100

    edit...

--- register.php
+++ register.php
@@ -7,13 +7,13 @@
     if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' )
     {
         # Insert first name.
-        $fn = mysqli_real_escape_string( $dbc, trim( $_POST[ 'fname' ] ) ) ;
+        $fn = mysqli_real_escape_string( $dbc, trim( $_POST[ 'first_name' ] ) ) ;
 
         # Insert other name.
-        $on = mysqli_real_escape_string( $dbc, trim( $_POST[ 'oname' ] ) ) ;
+        $on = mysqli_real_escape_string( $dbc, trim( $_POST[ 'other_name' ] ) ) ;
 
         # Insert last name.
-        $ln = mysqli_real_escape_string( $dbc, trim( $_POST[ 'lname' ] ) ) ;
+        $ln = mysqli_real_escape_string( $dbc, trim( $_POST[ 'last_name' ] ) ) ;
 
         # Insert gender.
         $g = mysqli_real_escape_string( $dbc, trim( $_POST[ 'gender' ] ) ) ;
@@ -77,7 +77,7 @@
                 <div class="form-container col-lg-12 col-sm-8">
                     <div class="media-container-column" data-form-type="formoid">
                         <!---Formbuilder Form--->
-                        <form action="https://save-data.php/" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="dIbVba7+ttPk0+hxXrJmb7JlcOjh/MWsNO6BzO/VT6DgWcC5J+qtWTiMqR+BGqIod1mLuVnsMG9BMqym+ZBYhaB/yDh6dAmDgtAgVAk2VtaLXbcru9lNTbiwlknnciOU">
+                        <form action="register.php" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="dIbVba7+ttPk0+hxXrJmb7JlcOjh/MWsNO6BzO/VT6DgWcC5J+qtWTiMqR+BGqIod1mLuVnsMG9BMqym+ZBYhaB/yDh6dAmDgtAgVAk2VtaLXbcru9lNTbiwlknnciOU">
                             <div class="row">
                                 <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks for filling out the form!</div>
                                 <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
@@ -87,67 +87,67 @@
                         <div class="row">
                             <div class="dragArea col-lg-4">
                                 <div class="col-md-12 form-group " data-for="fname">
-                                    <input type="text" name="first_name" placeholder="First Name" data-form-field="FName" required="required" class="form-control px-3 display-7" id="fname-header15-f">
+                                    <input type="text" name="first_name" placeholder="First Name" data-form-field="FName" required="required" class="form-control px-3 display-7" id="fname-header15-f" value="<?php if (isset($_POST['first_name'])) echo $_POST['first_name']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="oname">
-                                    <input type="text" name="other_name" placeholder="Other Name" data-form-field="OName" class="form-control px-3 display-7" id="oname-header15-f">
+                                    <input type="text" name="other_name" placeholder="Other Name" data-form-field="OName" class="form-control px-3 display-7" id="oname-header15-f" value="<?php if (isset($_POST['other_name'])) echo $_POST['other_name']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="lname">
-                                    <input type="text" name="last_name" placeholder="Last Name" data-form-field="LName" required="required" class="form-control px-3 display-7" id="lname-header15-f">
+                                    <input type="text" name="last_name" placeholder="Last Name" data-form-field="LName" required="required" class="form-control px-3 display-7" id="lname-header15-f" value="<?php if (isset($_POST['last_name'])) echo $_POST['last_name']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="gender">
-                                    <select name="gender" data-form-field="Gender" required="required" class="form-control px-3 display-7" id="gender-header15-f">
+                                    <select name="gender" data-form-field="Gender" required="required" class="form-control px-3 display-7" id="gender-header15-f" value="<?php if (isset($_POST['gender'])) echo $_POST['gender']; ?>">
                                         <option value="male">Choose your gender</option>
                                         <option value="male">Male</option>
                                         <option value="female">Female</option>
                                     </select>
                                 </div>
                                 <div class="col-md-12 form-group " data-for="email">
-                                    <input type="email" name="email" placeholder="Email" data-form-field="Email" required="required" class="form-control px-3 display-7" id="email-header15-f">
+                                    <input type="email" name="email" placeholder="Email" data-form-field="Email" required="required" class="form-control px-3 display-7" id="email-header15-f" value="<?php if (isset($_POST['email'])) echo $_POST['email']; ?>">
                                 </div>
                                 
                             </div>
                             <div class="dragArea col-lg-4">
                                 <div class="col-md-12 form-group " data-for="dob">
-                                    <input type="date" name="dob" value="2018-07-22" data-form-field="dob" required="required" class="form-control px-3 display-7" id="dob-header15-f" max = "2005-12-31">
+                                    <input type="date" name="dob" value="2018-07-22" data-form-field="dob" required="required" class="form-control px-3 display-7" id="dob-header15-f" max = "2005-12-31" value="<?php if (isset($_POST['dob'])) echo $_POST['dob']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="address1">
-                                    <input type="text" name="address1" placeholder="Address Field 1" data-form-field="address" required="required" class="form-control px-3 display-7" id="address1-header15-f">
+                                    <input type="text" name="address1" placeholder="Address Field 1" data-form-field="address" required="required" class="form-control px-3 display-7" id="address1-header15-f" value="<?php if (isset($_POST['address1'])) echo $_POST['address1']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="address2">
-                                    <input type="text" name="address2" placeholder="Address Field 2" data-form-field="address2" class="form-control px-3 display-7" id="address2-header15-f">
+                                    <input type="text" name="address2" placeholder="Address Field 2" data-form-field="address2" class="form-control px-3 display-7" id="address2-header15-f" value="<?php if (isset($_POST['address2'])) echo $_POST['address2']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="city">
-                                    <input type="text" name="city" placeholder="City" data-form-field="City" required="required" class="form-control px-3 display-7" id="city-header15-f">
+                                    <input type="text" name="city" placeholder="City" data-form-field="City" required="required" class="form-control px-3 display-7" id="city-header15-f" value="<?php if (isset($_POST['city'])) echo $_POST['city']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="state">
-                                    <input type="text" name="state" placeholder="State" data-form-field="state" required="required" class="form-control px-3 display-7" id="state-header15-f">
+                                    <input type="text" name="state" placeholder="State" data-form-field="state" required="required" class="form-control px-3 display-7" id="state-header15-f" value="<?php if (isset($_POST['state'])) echo $_POST['state']; ?>">
                                 </div>
                                 
                             </div>
                             <div class="dragArea col-lg-4">
                                 <div class="col-md-12 form-group " data-for="country">
-                                    <input type="text" name="country" placeholder="Country" data-form-field="Country" required="required" class="form-control px-3 display-7" id="country-header15-f">
+                                    <input type="text" name="country" placeholder="Country" data-form-field="Country" required="required" class="form-control px-3 display-7" id="country-header15-f" value="<?php if (isset($_POST['country'])) echo $_POST['country']; ?>">
                                     <input type="hidden" id="latitude_input" />
                                     <input type="hidden" id="longitude_input" />
                                 </div>
                                 <div class="col-md-12 form-group " data-for="phone1">
-                                    <input type="phone1" name="phone1" placeholder="Phone Number 1" data-form-field="phone1" required="required" class="form-control px-3 display-7" id="phone1-header15-f">
+                                    <input type="phone1" name="phone1" placeholder="Phone Number 1" data-form-field="phone1" required="required" class="form-control px-3 display-7" id="phone1-header15-f" value="<?php if (isset($_POST['phone1'])) echo $_POST['phone1']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="phone2">
-                                    <input type="text" name="phone2" placeholder="Phone Number 2" data-form-field="phone2" class="form-control px-3 display-7" id="phone2-header15-f">
+                                    <input type="text" name="phone2" placeholder="Phone Number 2" data-form-field="phone2" class="form-control px-3 display-7" id="phone2-header15-f" value="<?php if (isset($_POST['phone2'])) echo $_POST['phone2']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="nok">
-                                    <input type="text" name="nok" placeholder="Next of Kin" data-form-field="nok" required="required" class="form-control px-3 display-7" id="nok-header15-f">
+                                    <input type="text" name="nok" placeholder="Next of Kin" data-form-field="nok" required="required" class="form-control px-3 display-7" id="nok-header15-f" value="<?php if (isset($_POST['nok'])) echo $_POST['nok']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="note">
-                                    <input type="text" name="note" placeholder="Note: Optional" data-form-field="Note" class="form-control px-3 display-7" id="note-header15-f">
+                                    <input type="text" name="note" placeholder="Note: Optional" data-form-field="Note" class="form-control px-3 display-7" id="note-header15-f" value="<?php if (isset($_POST['note'])) echo $_POST['note']; ?>">
                                 </div>
                                 
                             </div>
                         </div>
                             <div class="col-md-12 input-group-btn align-center">
-                                <button type="submit" class="btn btn-secondary btn-form display-4">SUBMIT</button>
+                                <button type="submit" class="btn btn-secondary btn-form display-4" value="register">SUBMIT</button>
                             </div>
                         </form><!---Formbuilder Form--->
                     </div>
