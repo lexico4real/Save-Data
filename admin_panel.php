3f5699de41776108cb5a2e2e3ec0536eea3110e3
Abubakar Oluyinka <lexico4real@gmail.com>
2019-12-28 21:24:14 +0100

    edit...

---
+++ admin_panel.php
@@ -0,0 +1,26 @@
+<?php
+    $page_title = 'Admin Login' ;
+    include_once('admin_header.html');
+?>
+
+<section class="mbr-section form4 cid-rLAZIDX1vo" id="form4-i">
+
+    
+
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+            </div>
+            <div class="col-md-6">
+                <h2 class="pb-3 align-left mbr-fonts-style display-2 align-center" style="color: #ED3567;">
+                    ADMIN HOME
+                </h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+    include_once('footer.html');
+?>
