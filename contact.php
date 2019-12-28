97cbcc838151e685541af16597d1a2d89c636bde
Abubakar Oluyinka <lexico4real@gmail.com>
2019-12-28 18:06:59 +0100

    edit...

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@
             </div>
             <div class="col-md-6">
                 <h2 class="pb-3 align-left mbr-fonts-style display-2" style="color: #ED3567;">
-                    Drop a Line
+                    Drop a Message
                 </h2>
                 <div>
                     <div class="icon-block pb-3 align-left">
@@ -31,17 +31,48 @@
                             Ready for offers and cooperation
                         </h5>
                         <p class="mbr-text align-left mbr-fonts-style display-7">
-                            Phone: +1 (0) 000 0000 001 <br>
-                            Email: youremail@mail.com
+                            Phone: +234 (0) 000 0000 001 <br>
+                            Email: company@mail.com
                         </p>
                     </div>
                 </div>
-                <div data-form-type="formoid">
+                <div data-form-type="">
                     <!---Formbuilder Form--->
-                    <form action="https://mobirise.com/" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="z989lQIPVDn8X4bkWkX06aEM85LHFDtkTWntIjB9KrhsW8v1nM7Z/AlmTB8zg+pd+580954kTKmWuwByiN+n/aQd1K0FBFAUcFdmPs6aJ/kQROXvaH3kb87hMAAA9C5g">
+                    <form action="contact.php" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="z989lQIPVDn8X4bkWkX06aEM85LHFDtkTWntIjB9KrhsW8v1nM7Z/AlmTB8zg+pd+580954kTKmWuwByiN+n/aQd1K0FBFAUcFdmPs6aJ/kQROXvaH3kb87hMAAA9C5g">
                         <div class="row">
                             <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks for filling out the form!</div>
-                            <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
+                            <div class="col-12">
+                                <?php
+                                    if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' )
+                                    {
+                                      # Connect to the database.
+                                      require ('connect_db.php'); 
+                                    
+                                      # name.
+                                      $nm = mysqli_real_escape_string( $dbc, trim( $_POST[ 'name' ] ) ) ;
+                                    
+                                      # phone.
+                                      $ph = mysqli_real_escape_string( $dbc, trim( $_POST[ 'phone' ] ) ) ;
+                                    
+                                      # email address:
+                                      $e = mysqli_real_escape_string( $dbc, trim( $_POST[ 'email' ] ) ) ;
+
+                                      # message:
+                                      $m = mysqli_real_escape_string( $dbc, trim( $_POST[ 'message' ] ) ) ;
+                                      
+                                      # On success insert msg into 'contact_message' database table.
+                                      $q = "INSERT INTO contact_message (name, phone, email, message) VALUES ('$nm', '$ph', '$e', '$m')";
+                                        $r = @mysqli_query ( $dbc, $q ) ;
+                                        
+                                        echo '<div class="alert alert-success">
+                                                            <strong>Success!</strong> Thanks for your message. We will get back to you shortly!!!
+                                                        </div>';
+                                        # Close database connection.
+                                        mysqli_close($dbc); 
+                                    
+                                        exit();
+                                    }
+                                ?>
                             </div>
                         </div>
                         <div class="dragArea row">
@@ -55,7 +86,7 @@
                                 <input type="text" name="email" placeholder="Email" data-form-field="Email" class="form-control input display-7" required="required" id="email-form4-i">
                             </div>
                             <div data-for="message" class="col-md-12  form-group">
-                                <textarea name="message" placeholder="Message" data-form-field="Message" class="form-control input display-7" id="message-form4-i"></textarea>
+                                <textarea name="message" placeholder="Message" data-form-field="Message" class="form-control input display-7" id="message-form4-i" required="required"></textarea>
                             </div>
                             <div class="col-md-12 input-group-btn  mt-2 align-center">
                                 <button type="submit" class="btn btn-primary btn-form display-4">SEND MESSAGE</button>
