1dc40e07367b423ba479b87a82897c3d733916e9
Abubakar Oluyinka <lexico4real@gmail.com>
2019-12-29 12:20:53 +0100

    edit...

--- admin_panel.php
+++ admin_panel.php
@@ -22,40 +22,39 @@
                 <h2 class="pb-3 align-left mbr-fonts-style display-2 align-center" style="color: #ED3567;">
                     ADMIN HOME
                 </h2>
-                <table class="table table-dark" style="color:#ED3567">
+                <?php
+                            # Open database connection.
+                            require ( 'connect_db.php' ) ;
+
+                            # Retrieve items from 'register' database table.
+                            $q = "SELECT * FROM register" ;
+                            $r = mysqli_query( $dbc, $q ) ;
+                            if ( mysqli_num_rows( $r ) > 0 )
+                            {
+                echo '<table class="table table-dark" style="color:#ED3567;">
                     <thead>
-                    <tr>
-                        <th>S/N</th>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Email</th>
-                        <th>Detail</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                        <td data-toggle="tooltip" title="View full detail"><a href="detail.php"><button type="submit" style="padding:0px 15px; color:black; background-color: #ED3567; border-radius:5px;">View</button></a></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Mary</td>
-                        <td>Moe</td>
-                        <td>mary@example.com</td>
-                        <td data-toggle="tooltip" title="View full detail"><button type="submit" style="padding:0px 15px; color:black; background-color: #ED3567; border-radius:5px;">View</button></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>July</td>
-                        <td>Dooley</td>
-                        <td>july@example.com</td>
-                        <td data-toggle="tooltip" title="View full detail"><button type="submit" style="padding:0px 15px; color:black; background-color: #ED3567; border-radius:5px;">View</button></td>
-                    </tr>
-                    </tbody>
-                </table>
+                        <tr>
+                            <th>ID</th>
+                            <th>Firstname</th>
+                            <th>Lastname</th>
+                            <th>Email</th>
+                            <th>Detail</th>
+                        </tr>
+                    </thead>';
+                            echo '<tbody><tr>';
+                            while ( $row = mysqli_fetch_array( $r, MYSQLI_ASSOC ))
+                            {
+                                echo '<td>1</td>
+                                <td>John</td>
+                                <td>Doe</td>
+                                <td>john@example.com</td>
+                                <td data-toggle="tooltip" title="View full detail"><a href="detail.php"><button type="submit" style="padding:0px 15px; color:black; background-color: #ED3567; border-radius:5px;">View</button></a></td>';
+                            }
+                        }
+                            echo '</tr></tbody>';
+                echo '</table>';
+                mysqli_close( $dbc ) ; 
+                ?>
             </div>
         </div>
     </div>
