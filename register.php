7e66df42d23f7e20e52cc282ead2c29f5adf8c8b
Abubakar Oluyinka <lexico4real@gmail.com>
2020-01-05 22:45:01 +0100

    edit...

--- register.php
+++ register.php
@@ -9,14 +9,25 @@
     
 
     <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(7, 59, 76);"></div>
-
+    <style>
+		.imageCont{
+			width: 150px;
+			height: 200px;
+			background-image: url('back.jpg');
+            border: 1px solid #ED3567;
+		}
+        input[type='file'] {
+            background: white;    
+        }
+	</style>
     <div class="container">
         <div class="row">
             
                 <div class="form-container col-lg-12 col-sm-8">
                     <div class="media-container-column" data-form-type="">
                         <!---Formbuilder Form--->
-                        <form action="register.php" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="dIbVba7+ttPk0+hxXrJmb7JlcOjh/MWsNO6BzO/VT6DgWcC5J+qtWTiMqR+BGqIod1mLuVnsMG9BMqym+ZBYhaB/yDh6dAmDgtAgVAk2VtaLXbcru9lNTbiwlknnciOU">
+                        <form action="register.php" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" enctype="multipart/form-data" value="dIbVba7+ttPk0+hxXrJmb7JlcOjh/MWsNO6BzO/VT6DgWcC5J+qtWTiMqR+BGqIod1mLuVnsMG9BMqym+ZBYhaB/yDh6dAmDgtAgVAk2VtaLXbcru9lNTbiwlknnciOU">
+                            <input type="hidden" name="size" />
                             <div class="row">
                                 <div class="col-12">
                                     <?php
@@ -96,6 +107,13 @@
                                 </div>
                             </div>
                             <h1 class="align-center">Fill the form below</h1><br>
+                            <div class="align-center">
+                                <div class="col-md-12 form-group " data-for="img">
+                                    <img id="blah" src="#" class="imageCont"><br /><br />
+                                    <input type="file" id="img-header15-f" style="color:transparent; width:100px;" name="image" id="image" onchange="readURL(this);" required /><br />
+                                </div>
+                            </div>
+                            <br />
                         <div class="row">
                             <div class="dragArea col-lg-4">
                                 <div class="col-md-12 form-group " data-for="fname">
@@ -124,36 +142,36 @@
                                     <input type="text" name="dob" placeholder="Date of Birth" onfocus="(this.type='date')" data-form-field="dob" required="required" class="form-control px-3 display-7" id="dob-header15-f" max = "2005-12-31" value="<?php if (isset($_POST['dob'])) echo $_POST['dob']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="address1">
-                                    <input type="text" name="address1" placeholder="Address Field 1" data-form-field="address" required="required" class="form-control px-3 display-7" id="address1-header15-f" value="<?php if (isset($_POST['address1'])) echo $_POST['address1']; ?>">
+                                    <input type="text" name="address1" placeholder="State" data-form-field="address" required="required" class="form-control px-3 display-7" id="address1-header15-f" value="<?php if (isset($_POST['address1'])) echo $_POST['address1']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="address2">
-                                    <input type="text" name="address2" placeholder="Address Field 2" data-form-field="address2" class="form-control px-3 display-7" id="address2-header15-f" value="<?php if (isset($_POST['address2'])) echo $_POST['address2']; ?>">
+                                    <input type="text" name="address2" placeholder="LGA" data-form-field="address2" class="form-control px-3 display-7" id="address2-header15-f" value="<?php if (isset($_POST['address2'])) echo $_POST['address2']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="city">
-                                    <input type="text" name="city" placeholder="City" data-form-field="City" required="required" class="form-control px-3 display-7" id="city-header15-f" value="<?php if (isset($_POST['city'])) echo $_POST['city']; ?>">
+                                    <input type="text" name="city" placeholder="Ward" data-form-field="City" required="required" class="form-control px-3 display-7" id="city-header15-f" value="<?php if (isset($_POST['city'])) echo $_POST['city']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="state">
-                                    <input type="text" name="state" placeholder="State" data-form-field="state" required="required" class="form-control px-3 display-7" id="state-header15-f" value="<?php if (isset($_POST['state'])) echo $_POST['state']; ?>">
+                                    <input type="text" name="state" placeholder="Polling Unit" data-form-field="state" required="required" class="form-control px-3 display-7" id="state-header15-f" value="<?php if (isset($_POST['state'])) echo $_POST['state']; ?>">
                                 </div>
                                 
                             </div>
                             <div class="dragArea col-lg-4">
                                 <div class="col-md-12 form-group " data-for="country">
-                                    <input type="text" name="country" placeholder="Country" data-form-field="Country" required="required" class="form-control px-3 display-7" id="country-header15-f" value="<?php if (isset($_POST['country'])) echo $_POST['country']; ?>">
+                                    <input type="text" name="country" placeholder="Polling Unit ID (Optional)" data-form-field="Country" class="form-control px-3 display-7" id="country-header15-f" value="<?php if (isset($_POST['country'])) echo $_POST['country']; ?>">
                                     <input type="hidden" id="latitude_input" />
                                     <input type="hidden" id="longitude_input" />
                                 </div>
                                 <div class="col-md-12 form-group " data-for="phone1">
-                                    <input type="phone1" name="phone1" placeholder="Phone Number 1" data-form-field="phone1" required="required" class="form-control px-3 display-7" id="phone1-header15-f" value="<?php if (isset($_POST['phone1'])) echo $_POST['phone1']; ?>">
+                                    <input type="tel" name="phone1" placeholder="Phone Number 1" data-form-field="phone1" required="required" class="form-control px-3 display-7" id="phone1-header15-f" value="<?php if (isset($_POST['phone1'])) echo $_POST['phone1']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="phone2">
-                                    <input type="text" name="phone2" placeholder="Phone Number 2" data-form-field="phone2" class="form-control px-3 display-7" id="phone2-header15-f" value="<?php if (isset($_POST['phone2'])) echo $_POST['phone2']; ?>">
+                                    <input type="tel" name="phone2" placeholder="Phone Number 2" data-form-field="phone2" class="form-control px-3 display-7" id="phone2-header15-f" value="<?php if (isset($_POST['phone2'])) echo $_POST['phone2']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="nok">
-                                    <input type="text" name="nok" placeholder="Next of Kin" data-form-field="nok" required="required" class="form-control px-3 display-7" id="nok-header15-f" value="<?php if (isset($_POST['nok'])) echo $_POST['nok']; ?>">
+                                    <input type="text" name="nok" placeholder="Voter Identification No. (VIN)" data-form-field="nok" required="required" class="form-control px-3 display-7" id="nok-header15-f" value="<?php if (isset($_POST['nok'])) echo $_POST['nok']; ?>">
                                 </div>
                                 <div class="col-md-12 form-group " data-for="note">
-                                    <input type="text" name="note" placeholder="Note: Optional" data-form-field="Note" class="form-control px-3 display-7" id="note-header15-f" value="<?php if (isset($_POST['note'])) echo $_POST['note']; ?>">
+                                    <input type="text" name="note" placeholder="Note (Optional)" data-form-field="Note" class="form-control px-3 display-7" id="note-header15-f" value="<?php if (isset($_POST['note'])) echo $_POST['note']; ?>">
                                 </div>
                                 
                             </div>
@@ -168,7 +186,22 @@
         </div>
     </div>
 </section>
+<script>
+	function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
 
 <?php
     include_once('footer.html');
